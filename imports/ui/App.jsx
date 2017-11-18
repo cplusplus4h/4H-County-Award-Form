9ba604b1aa8615bb1b50dfe3f1ba16c2de82483d
harshwinds <emily.harsh@wolfprint.com>
2017-11-17 22:59:58 -0500

    Add React for client side development

---
+++ imports/ui/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// App component - represents the whole app
+export default class App extends Component {
+ render() {
+   return (
+     <div className="container">
+       <header>
+         <h1>4-H County Award Form</h1>
+       </header>
+     </div>
+   );
+ }
+}
